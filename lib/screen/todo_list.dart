f8ca001a3d84ce8a0c5ce2793f8d6263ce398925
kangsudal_notebook <kangsudal@gmail.com>
2022-09-19 20:15:54 +0900

    display data and pull to refresh

--- lib/screen/todo_list.dart
+++ lib/screen/todo_list.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:todo_app_202209/screen/add_todo.dart';
 import 'package:http/http.dart' as http;
@@ -10,6 +12,7 @@ class TodoListPage extends StatefulWidget {
 }
 
 class _TodoListPageState extends State<TodoListPage> {
+  List items = [];
   @override
   void initState() {
     super.initState();
@@ -21,6 +24,19 @@ class _TodoListPageState extends State<TodoListPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Todo List"),
+      ),
+      body: RefreshIndicator(
+        onRefresh: fetchTodo,
+        child: ListView.builder(
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              final item = items[index] as Map;
+              return ListTile(
+                leading: CircleAvatar(child: Text('${index+1}')),
+                title: Text(item['title']),
+                subtitle: Text(item['description']),
+              );
+            }),
       ),
       floatingActionButton: FloatingActionButton.extended(
           onPressed: navigateToAddPage, label: Text('Add Todo')),
@@ -40,6 +56,14 @@ class _TodoListPageState extends State<TodoListPage> {
     final url = "https://api.nstack.in/v1/todos?page=1&limit=10";
     final uri = Uri.parse(url);
     final response = await http.get(uri);
-    print(response.body);
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body) as Map;
+      final result = json['items'] as List;
+      setState(() {
+        items = result;
+      });
+    } else {
+      //error
+    }
   }
 }
