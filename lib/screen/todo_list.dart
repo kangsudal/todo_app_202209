5feff64e06b5474ad6057dc22ab347f43c2efbc9
kangsudal_notebook <kangsudal@gmail.com>
2022-09-19 19:46:51 +0900

    get response data

--- lib/screen/todo_list.dart
+++ lib/screen/todo_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app_202209/screen/add_todo.dart';
+import 'package:http/http.dart' as http;
 
 class TodoListPage extends StatefulWidget {
   const TodoListPage({Key? key}) : super(key: key);
@@ -9,6 +10,12 @@ class TodoListPage extends StatefulWidget {
 }
 
 class _TodoListPageState extends State<TodoListPage> {
+  @override
+  void initState() {
+    super.initState();
+    fetchTodo();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,5 +34,12 @@ class _TodoListPageState extends State<TodoListPage> {
       },
     );
     Navigator.push(context, route);
+  }
+
+  Future<void> fetchTodo() async {
+    final url = "https://api.nstack.in/v1/todos?page=1&limit=10";
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    print(response.body);
   }
 }
