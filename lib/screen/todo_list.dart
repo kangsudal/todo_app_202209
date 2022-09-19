9b3c3b7c3d1c4ebb3a35a3125f4050bde386a98d
kangsudal_notebook <kangsudal@gmail.com>
2022-09-19 23:42:15 +0900

    Add Visibility widget

--- lib/screen/todo_list.dart
+++ lib/screen/todo_list.dart
@@ -12,6 +12,7 @@ class TodoListPage extends StatefulWidget {
 }
 
 class _TodoListPageState extends State<TodoListPage> {
+  bool isLoading = true;
   List items = [];
   @override
   void initState() {
@@ -25,18 +26,22 @@ class _TodoListPageState extends State<TodoListPage> {
       appBar: AppBar(
         title: Text("Todo List"),
       ),
-      body: RefreshIndicator(
-        onRefresh: fetchTodo,
-        child: ListView.builder(
-            itemCount: items.length,
-            itemBuilder: (context, index) {
-              final item = items[index] as Map;
-              return ListTile(
-                leading: CircleAvatar(child: Text('${index+1}')),
-                title: Text(item['title']),
-                subtitle: Text(item['description']),
-              );
-            }),
+      body: Visibility(
+        visible: isLoading,
+        child: Center(child: CircularProgressIndicator(),),
+        replacement: RefreshIndicator(
+          onRefresh: fetchTodo,
+          child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                final item = items[index] as Map;
+                return ListTile(
+                  leading: CircleAvatar(child: Text('${index+1}')),
+                  title: Text(item['title']),
+                  subtitle: Text(item['description']),
+                );
+              }),
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
           onPressed: navigateToAddPage, label: Text('Add Todo')),
@@ -65,5 +70,8 @@ class _TodoListPageState extends State<TodoListPage> {
     } else {
       //error
     }
+    setState(() {
+      isLoading = false;
+    });
   }
 }
