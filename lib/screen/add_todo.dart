bdcf25a8694e72c6f84107056dce0538cffd59e3
kangsudal_notebook <kangsudal@gmail.com>
2022-09-19 15:07:57 +0900

    get the data from form

--- lib/screen/add_todo.dart
+++ lib/screen/add_todo.dart
@@ -8,12 +8,51 @@ class AddTodoPage extends StatefulWidget {
 }
 
 class _AddTodoPageState extends State<AddTodoPage> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Add Todo"),
       ),
+      body: ListView(
+        padding: EdgeInsets.all(20),
+        children: [
+          TextField(
+            controller: titleController,
+            decoration: InputDecoration(hintText: 'title'),
+          ),
+          TextField(
+            controller: descriptionController,
+            decoration: InputDecoration(hintText: 'description'),
+            maxLines: 8,
+            minLines: 3,
+            // keyboardType: TextInputType.multiline,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Submit'),
+          ),
+        ],
+      ),
     );
+  }
+
+  void submitData() {
+    //get the data from form
+    final title = titleController.text;
+    final description = descriptionController.text;
+    final body = {
+      "title": title,
+      "description": description,
+      "is_completed": false
+    };
+    //submit data to the server
+    //show success or fail based on status
   }
 }
